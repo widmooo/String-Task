8b99a2cdc29c72cf7765e2d552ca0dd6db3eaabb
Piotr Widomski <40829892+widmooo@users.noreply.github.com>
2020-11-08 22:53:49 +0100

    Add files via upload

---
+++ string_task_codewars.py
@@ -0,0 +1,15 @@
+def string_task(s):
+    blocked = ["a", "o", "y", "e", "u", "i"]
+    s = s.lower()
+    letters = list(s)
+
+    for i in letters:
+        if i in blocked:
+            letters.remove(i)
+        listToStr = '.'.join([str(i) for i in letters])
+        result = "." + listToStr
+    print(result)
+    return(result)
+
+
+string_task("Tour")  # WHY IT RETURN WITH "U" LETTER!??!
